f820f66f1e8d329b35ba582a150e6e59d8e1fd3e
J. Gabriel Maia <jonagabriel@gmail.com>
2020-04-18 19:51:54 -0300

    New Incident page and style

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes(){
     return(
@@ -11,6 +12,7 @@ export default function Routes(){
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     )
