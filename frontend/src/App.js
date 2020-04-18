d72e583217e33d5e234b487fa1ab43be26e19fba
J. Gabriel Maia <jonagabriel@gmail.com>
2020-04-18 16:15:38 -0300

    Register page and styke

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Logon from './pages/Logon';
+import Routes from './routes';
 import './global.css';
 
 function App() {
   return (
-   <Logon/>
+   <Routes/>
   );
 }
 
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi'; 
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
@@ -17,9 +18,9 @@ export default function Logon (){
                     <input placeholder="Sua ID"/>
                     <button type="submit" className="button">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
-                        Não tenho cadastro</a>
+                        Não tenho cadastro</Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
