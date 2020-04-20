bf309f7a349ffe46f5d5e50917ac4e152db138d4
J. Gabriel Maia <jonagabriel@gmail.com>
2020-04-20 00:36:00 -0300

    Profile page handlers

--- backend/src/controller/ProfileController.js
+++ backend/src/controller/ProfileController.js
@@ -7,7 +7,7 @@ module.exports = {
             .where('ong_id', ong_id)
             .select('*');
 
-        return response.json({incidents});
+        return response.json(incidents);
                     
     }
 }
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi'; 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon (){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+        
+        try{
+            const response = await api.post('session', { id } );
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        }
+        catch(err){
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the hero"/>
 
-                <form action="">
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)}/>
                     <button type="submit" className="button">Entrar</button>
 
                     <Link className="back-link" to="/register">
